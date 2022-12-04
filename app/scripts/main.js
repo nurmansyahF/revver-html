a85a60719303b48ef1d2fb87e9b4112102676fac
nurmansyahF <faisal.nurmansyah13@gmail.com>
2022-12-04 09:01:45 +0700

    Update Page
    Update Page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -63,7 +63,8 @@
           navs = t.attr('data-nav') && t.attr('data-nav') == "yes" ? true : false,
 					centers = t.attr('data-center') && t.attr('data-center') == "yes" ? true : false,
 					loops = t.attr('data-loop') && t.attr('data-loop') == "no" ? false : true,
-          dot = t.attr('data-dot') && t.attr('data-dot') == "no" ? false : true;
+          dot = t.attr('data-dot') && t.attr('data-dot') == "no" ? false : true,
+          width = t.attr('data-width') && t.attr('data-width') == "no" ? false : true;
 
       if (nItem > 1){
         t.addClass('owl-carousel');
@@ -72,6 +73,7 @@
           loop: loops,
           dots: dot,
           nav: navs,
+          autoWidth: width,
           margin: 24,
           center: true,
           navText: ["<span class='icon-chevron-left'></span>","<span class='icon-chevron-right'></span>"],
@@ -180,6 +182,67 @@
       fixedHeader();
       // mastParallax();
       sliderXS();
+    });
+
+    // Btn Qty
+    $('.btn-number').click(function(e){
+      e.preventDefault();
+
+      var
+          fieldName = $(this).attr('data-field'),
+          type      = $(this).attr('data-type'),
+          input = $("input[name='"+fieldName+"']"),
+          currentVal = parseInt(input.val());
+      if (!isNaN(currentVal)) {
+          if(type == 'minus') {
+
+              if(currentVal > input.attr('min')) {
+                  input.val(currentVal - 1).change();
+              }
+              if(parseInt(input.val()) == input.attr('min')) {
+                  $(this).attr('disabled', true);
+              }
+
+          } else if(type == 'plus') {
+
+              if(currentVal < input.attr('max')) {
+                  input.val(currentVal + 1).change();
+              }
+              if(parseInt(input.val()) == input.attr('max')) {
+                  $(this).attr('disabled', true);
+              }
+
+          }
+      } else {
+          input.val(0);
+      }
+    });
+    $('.input-number').focusin(function(){
+      $(this).data('oldValue', $(this).val());
+    });
+    $('.input-number').change(function() {
+
+        var minValue =  parseInt($(this).attr('min')),
+        maxValue =  parseInt($(this).attr('max')),
+        valueCurrent = parseInt($(this).val());
+
+        var name = $(this).attr('name');
+        if(valueCurrent >= minValue) {
+            $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            alert('Sorry, the minimum value was reached');
+            $(this).val($(this).data('oldValue'));
+        }
+        if(valueCurrent <= maxValue) {
+            $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            alert('Sorry, the maximum value was reached');
+            $(this).val($(this).data('oldValue'));
+        }
+    });
+
+    $('.btn-number').click(function(e){
+      e.preventDefault();
     });
 
   }
